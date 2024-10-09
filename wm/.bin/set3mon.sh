46a6a58252d2cb0248157945d40ffa09fbfb524d
thisisnotcamilo <vargasrcamilo@gmail.com>
2024-10-09 12:23:41 -0300

    add: 3-mon script

---
+++ wm/.bin/set3mon.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --mode 1920x1080 --pos 3640x0 --rotate right --output DisplayPort-1 --primary --mode 2560x1440 --pos 1080x240 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate left
